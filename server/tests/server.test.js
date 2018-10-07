1cecd699368e006225181e1f311546fd0694a613
qronix <qroniix@gmail.com>
2018-10-06 21:48:00 -0400

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,60 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done)=>{
+    Todo.remove({}).then(()=>{
+        done();
+    });
+});
+
+describe('POST /todos',()=>{
+    it('should create a new todo', (done)=>{
+        var text = "Test todo text";
+
+        request(app)
+        .post('/todos')
+        .send({text})
+        .expect(200)
+        .expect((res)=>{
+           expect(res.body.text).toBe(text);
+        })
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+
+            Todo.find().then((todos)=>{
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch((err)=>{
+                done(err);
+            });
+        });
+    });
+    it('should not create a todo with invalid data',(done)=>{
+        //send empty data
+        //expect a 400
+        //check for errors
+        //verify the database does not contain any documents
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err,res)=>{
+            if(err){
+                return done(err);
+            }
+
+            Todo.find().then((todos)=>{
+                expect(todos.length).toBe(0);
+                done();
+            }).catch((err)=>{
+                done(err);
+            });
+        });
+    });
+});
