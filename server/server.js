dd071812099471a65a3593cde88f8a13b81f9078
qronix <qroniix@gmail.com>
2018-10-10 10:00:10 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const {ObjectID} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 var app = express();
 
@@ -77,7 +78,32 @@ app.delete('/todos/:id',(req,res)=>{
             //if doc, send back with 200
         //error
             //400 with empty body
-})
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(555).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((err)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`);
